33854d059107f664503ae1d0694eebfe75100acf
SageLady <53543677+SageLady@users.noreply.github.com>
2023-02-15 15:40:35 -0800

    Initial Commit - no file changes from initial creation of files

--- src/main/java/rmesser/dataxfr/MainMenuController.java
+++ src/main/java/rmesser/dataxfr/MainMenuController.java
@@ -3,7 +3,7 @@ package rmesser.dataxfr;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainMenuController {
     @FXML
     private Label welcomeText;
 
