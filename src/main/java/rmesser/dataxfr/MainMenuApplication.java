6cea5fb20d000915396bb1c52cf28d84d39d8025
SageLady <53543677+SageLady@users.noreply.github.com>
2023-02-15 16:27:16 -0800

    Rename and resize of main window

--- src/main/java/rmesser/dataxfr/MainMenuApplication.java
+++ src/main/java/rmesser/dataxfr/MainMenuApplication.java
@@ -11,8 +11,8 @@ public class MainMenuApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainMenuApplication.class.getResource("main-menu.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 800, 500);
+        stage.setTitle("Data Transfer Application");
         stage.setScene(scene);
         stage.show();
     }
